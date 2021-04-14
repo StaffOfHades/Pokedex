7982edb9644d96b736bb99bbcf4453c7949ddb2a
Mauricio Graciano <mau.graci@gmail.com>
2021-04-14 00:42:12 -0500

    Updated pokemon entry to add button to go back to route

--- src/views/PokemonEntry.vue
+++ src/views/PokemonEntry.vue
@@ -1,6 +1,27 @@
 <template>
   <v-container fluid>
-    <v-card v-if="pokemon !== undefined" class="text-center">
+    <v-card v-if="pokemon !== undefined">
+      <v-btn
+        class="mb-4 d-inline-flex d-sm-none d-md-none d-lg-none d-xl-none"
+        color="grey darken-2"
+        dark
+        href="/"
+        style="left: 16px; top: 16px"
+      >
+        <v-icon dark left> mdi-arrow-left </v-icon>Back
+      </v-btn>
+      <v-btn
+        absolute
+        class="d-none d-sm-inline-flex d-md-inline-flex d-lg-inline-flex d-xl-inline-flex"
+        color="grey darken-2"
+        dark
+        href="/"
+        left
+        top
+      >
+        <v-icon dark left> mdi-arrow-left </v-icon>Back
+      </v-btn>
+
       <!-- The pokemon data might not be loaded, so use a placeholder if the sprites are not yet available. -->
       <!-- Additionally, use the default sprite as placeholde to load the official artwork -->
       <v-img
@@ -31,8 +52,10 @@
         {{ pokemon.name }}
       </v-card-title>
 
-      <v-card-subtitle v-if="pokemon.id !== undefined"> #{{ pokemon.id }} </v-card-subtitle>
-      <v-card-subtitle v-else> &nbsp; </v-card-subtitle>
+      <v-card-subtitle v-if="pokemon.id !== undefined" class="text-center">
+        #{{ pokemon.id }}
+      </v-card-subtitle>
+      <v-card-subtitle v-else class="text-center"> &nbsp; </v-card-subtitle>
 
       <v-card-text v-if="pokemon.types !== undefined" class="text-center">
         <v-chip v-for="type in pokemon.types" :key="type.slot" class="ma-2" label>
@@ -46,12 +69,12 @@
       <v-divider class="mx-4"></v-divider>
 
       <v-card-title class="justify-center"> estadísticas </v-card-title>
-      <v-card-subtitle class="justify-center">
+      <v-card-subtitle class="text-center">
         valor máximo nivel 1 - valor mínimo nivel 100
       </v-card-subtitle>
 
       <v-card-text v-for="stat in stats" :key="stat.stat.name" class="text-center">
-        <label class="v-label theme--light mb-12 mb-sm-12 mb-md-0 d-block">{{
+        <label class="v-label theme--light mb-12 mb-sm-12 mb-md-0 mb-lg-0 mb-xl-0 d-block">{{
           stat.stat.name
         }}</label>
         <v-range-slider
